ad6394f30c8253bcef5ac834f52c0997a474b3fc
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-11-18 15:58:21 -0300

    eliminacion de codigo innecesario del main

--- Backend/src/main/java/fi/uba/memo1/apirest/finanzas/FinanzasApplication.java
+++ Backend/src/main/java/fi/uba/memo1/apirest/finanzas/FinanzasApplication.java
@@ -3,7 +3,6 @@ package fi.uba.memo1.apirest.finanzas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
 //import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // activar para no usar base de datos
@@ -11,9 +10,6 @@ import java.util.UUID;
 public class FinanzasApplication {
 
 	public static void main(String[] args) {
-		String id = "a6e2167f-67a1-4f60-b9e9-6bae7bc3a15";
-		UUID uuid = UUID.fromString(id);
-		System.out.println("UUID válido: " + uuid);
 		SpringApplication.run(FinanzasApplication.class, args);
 	}
 }
