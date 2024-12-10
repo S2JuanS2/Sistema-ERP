9fd50656813d25d264cf91fe18b1a71bc08bddef
Matias Vallejos <matiavallejo@gmail.com>
2024-12-10 20:21:44 -0300

    chore: mejor documentación api

--- Backend/src/main/java/fi/uba/memo1/apirest/finanzas/config/OpenApiConfig.java
+++ Backend/src/main/java/fi/uba/memo1/apirest/finanzas/config/OpenApiConfig.java
@@ -1,27 +1,17 @@
 package fi.uba.memo1.apirest.finanzas.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Finanzas API")
-                        .description("API para la gestión de costos mensuales")
-                        .version("v1"));
-    }
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch("/api/v1/finanzas/**")
-                .build();
+    RouterFunction<ServerResponse> staticResourceRouter(){
+        return RouterFunctions.resources("/openapi*", new ClassPathResource("static/"));
     }
 }
--- Backend/src/main/java/fi/uba/memo1/apirest/finanzas/controller/CostosMensualesController.java
+++ Backend/src/main/java/fi/uba/memo1/apirest/finanzas/controller/CostosMensualesController.java
@@ -4,12 +4,7 @@ import fi.uba.memo1.apirest.finanzas.dto.CostosMensualesRequest;
 import fi.uba.memo1.apirest.finanzas.dto.CostosMensualesResponse;
 import fi.uba.memo1.apirest.finanzas.dto.TotalCostosProyectoResponse;
 import fi.uba.memo1.apirest.finanzas.dto.CostoRequest;
-import fi.uba.memo1.apirest.finanzas.exception.RolNoEncontradoException;
 import fi.uba.memo1.apirest.finanzas.service.CostosMensualesService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,46 +23,30 @@ public class CostosMensualesController {
         this.service = service;
     }
 
-    @Operation(summary = "Obtener todos los costos mensuales")
-    @ApiResponse(responseCode = "200", description = "Costos mensuales encontrados")
     @GetMapping("/costos")
     public ResponseEntity<Mono<List<CostosMensualesResponse>>> getCostos() {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
     }
-
-    @Operation(summary = "Obtener un costo mensual por id")
-    @ApiResponse(responseCode = "200", description = "Costo mensual encontrado")
-    @ApiResponse(responseCode = "404", description = "Costo mensual no encontrado, rol invalido", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RolNoEncontradoException.class)))
     @GetMapping("/costos/{id}")
     public ResponseEntity<Mono<CostosMensualesResponse>> getCostos(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 
-    @Operation(summary = "Cargar un costo mensual")
-    @ApiResponse(responseCode = "201", description = "Costo mensual cargado")
-    @ApiResponse(responseCode = "404", description = "Error en la carga del costo mensual", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RolNoEncontradoException.class)))
     @PostMapping("/cargar-costo")
     public ResponseEntity<Mono<List<CostosMensualesResponse>>> cargarCosto(@RequestBody List<CostosMensualesRequest> costosMensualesRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(costosMensualesRequest));
     }
 
-    @Operation(summary = "Actualizar un costo mensual")
-    @ApiResponse(responseCode = "200", description = "Costo mensual actualizado")
-    @ApiResponse(responseCode = "400", description = "Error en la actualización del costo mensual, el costo no puede ser negativo",  content = @Content(mediaType = "application/json", schema = @Schema(implementation = RolNoEncontradoException.class)))
     @PutMapping("/actualizar-costo/{id}")
     public ResponseEntity<Mono<CostosMensualesResponse>> actualizarCosto(@PathVariable Long id, @RequestBody CostoRequest costoRequest){
         return ResponseEntity.status(HttpStatus.OK).body(service.update(id, costoRequest));
     }
 
-    @Operation(summary = "Actualizar todos los costos mensuales")
-    @ApiResponse(responseCode = "200", description = "Costos mensuales actualizados")
-    @ApiResponse(responseCode = "400", description = "Error en la actualización del costo mensual, el costo no puede ser negativo",  content = @Content(mediaType = "application/json", schema = @Schema(implementation = RolNoEncontradoException.class)))
     @PutMapping("/actualizar-costos")
     public ResponseEntity<Mono<List<CostosMensualesResponse>>> actualizarCostos(@RequestBody Map<Long, CostoRequest> costosRequest){
         return ResponseEntity.status(HttpStatus.OK).body(service.updateAll(costosRequest));
     }
     
-    @Operation(summary = "Dado un año, obtener los costos mes a mes del proyecto")
     @GetMapping("/proyectos/{anio}")
     public ResponseEntity<Mono<TotalCostosProyectoResponse>> getProyectos(@PathVariable String anio){
         return ResponseEntity.status(HttpStatus.OK).body(service.obtenerCostosDeProyectos(anio));
